d6e7cc0960afdeb68674f03571516e869486dbc0
edufsantos <eduardosantosifms@gmail.com>
2020-05-22 15:13:33 -0400

    Cadastro de Usúarios

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User'
+import UserController from './app/controller/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'edu',
-    email: 'edu@gmail.com',
-    password_hash: '1234556'
-  });
-  
-  res.json(user);
-});
+routes.post('/users', UserController.store);
+
 export default routes;
